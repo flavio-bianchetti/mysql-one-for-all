89c57bb97ef5999f6951e1bc65af8f09cc761c9b
Flávio Bianchetti <fjb.developer@gmail.com>
2022-02-04 09:15:26 -0300

    Add: Requirement 7 completed

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT
+  DISTINCT art.name AS `artista`,
+  alb.name AS `album`,
+  COUNT(ua.artist_id) AS `seguidores`
+FROM SpotifyClone.ARTIST AS art
+  INNER JOIN SpotifyClone.ALBUM as alb
+    ON alb.artist_id = art.id
+  INNER JOIN SpotifyClone.USER_ARTIST AS ua
+    ON ua.artist_id = art.id
+GROUP BY ua.artist_id, alb.name
+ORDER BY `seguidores` DESC;
